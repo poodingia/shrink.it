f04670f3804f5bcd2b37ec0988c18f3eda799720
ndtd128 <95179216+ndtd128@users.noreply.github.com>
2024-10-30 18:07:11 +0700

    feat: add key database and key populate

---
+++ keygeneration/src/main/java/org/uetmydinh/keygeneration/entity/Key.java
@@ -0,0 +1,15 @@
+package org.uetmydinh.keygeneration.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "keys")
+@Data
+@AllArgsConstructor
+public class Key {
+    @Id
+    private String id;
+    private Boolean isUsed;
+}
