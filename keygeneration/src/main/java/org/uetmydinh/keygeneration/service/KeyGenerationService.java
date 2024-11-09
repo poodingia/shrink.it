a0f0cac33a78eb725f6502ab784a5224f5b55923
Hien Doan <dkhien.work@gmail.com>
2024-11-09 23:40:40 +0700

    fix: use MongoDB findAndModify to avoid race condition in concurrent shortening requests

--- keygeneration/src/main/java/org/uetmydinh/keygeneration/service/KeyGenerationService.java
+++ keygeneration/src/main/java/org/uetmydinh/keygeneration/service/KeyGenerationService.java
@@ -1,6 +1,10 @@
 package org.uetmydinh.keygeneration.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 import org.uetmydinh.keygeneration.entity.Key;
 import org.uetmydinh.keygeneration.repository.KeyRepository;
@@ -10,16 +14,23 @@ import java.util.Optional;
 @Service
 public class KeyGenerationService {
     private final KeyRepository keyRepository;
+    private final MongoTemplate mongoTemplate;
+
 
     @Autowired
-    public KeyGenerationService(KeyRepository keyRepository) {
+    public KeyGenerationService(KeyRepository keyRepository, MongoTemplate mongoTemplate) {
         this.keyRepository = keyRepository;
+        this.mongoTemplate = mongoTemplate;
     }
 
     public Optional<Key> getAvailableKey() {
-        return keyRepository.findFirstByIsUsedFalse();
+        Query query = new Query(Criteria.where("isUsed").is(false));
+        Update update = new Update().set("isUsed", true);
+        Key key = mongoTemplate.findAndModify(query, update, Key.class);
+        return Optional.ofNullable(key);
     }
 
+    @Deprecated
     public void markKeyAsUsed(Key key) {
         key.setIsUsed(true);
         keyRepository.save(key);
