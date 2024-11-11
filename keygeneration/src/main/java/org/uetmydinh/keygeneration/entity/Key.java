0aae44229170ab2804c2be2351e2d783971b3960
Hien Doan <dkhien.work@gmail.com>
2024-11-12 02:20:05 +0700

    feat: add index for isUsed as we query based on it

--- keygeneration/src/main/java/org/uetmydinh/keygeneration/entity/Key.java
+++ keygeneration/src/main/java/org/uetmydinh/keygeneration/entity/Key.java
@@ -3,6 +3,7 @@ package org.uetmydinh.keygeneration.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "keys")
@@ -11,5 +12,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class Key {
     @Id
     private String id;
+
+    @Indexed
     private Boolean isUsed;
 }
